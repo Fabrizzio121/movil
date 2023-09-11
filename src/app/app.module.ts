b8e82ea76965c19bbc85ae0da6081f89712dab16
Fabrizzio121 <103144139+Fabrizzio121@users.noreply.github.com>
2023-09-10 21:15:49 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
-
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
+import { NgxQRCodeModule } from 'ngx-qrcode2'; 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot(),ZXingScannerModule,],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot(),ZXingScannerModule,QRScanner,NgxQRCodeModule,],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    QRScanner, 
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
